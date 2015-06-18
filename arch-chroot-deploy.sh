3f5becac013653d5bc56a98e7365357537e6fd1d
Ryan - David Reyes <ryan.david.reyes@gmail.com>
2015-06-18 17:39:21 -0400

    Updated scripts for spelling and noconfirm.
    pacman does not require interactive confirmation. Fixed spelling errors
    for reflector and added -i switch for pacstrap.

--- arch-chroot-deploy.sh
+++ arch-chroot-deploy.sh
@@ -6,9 +6,9 @@ locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 export LANG=en_US.UTF-8
 mkinitcpio -p linux
-pacman -S dialog wifi-menu os-prober grub iw wpa_supplicant wpa_actiond
+pacman -S dialog wifi-menu os-prober grub iw wpa_supplicant wpa_actiond --noconfirm
 systemctl enable netctl-auto@wlp2s0.service
 grub-install --target=i386-pc --recheck /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
-pacman -S laptop-mode-tools xorg-server xf86-video-intel git
+pacman -S tlp xorg-server xf86-video-intel git --noconfirm
 passwd
--- arch-deploy.sh
+++ arch-deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-pacman -Syu
-pacman -Sy reflector
+pacman -Syu --noconfirm
+pacman -Sy reflector --noconfirm
 cp -vf /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-reflector --verbosee --country 'United States' -l 10 --sort rate --save /etc/pacman.d/mirrorlist
-pacstrap /mnt base base_devel
-genfstab -p /mnt >> /mnt /etc/fstab
+reflector --verbose --country 'United States' -p http -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+pacstrap -i /mnt base base_devel
+genfstab -p /mnt >> /mnt/etc/fstab
 cp arch-chroot-deploy.sh /mnt
 arch-chroot /mnt arch-chroot-deploy.sh
