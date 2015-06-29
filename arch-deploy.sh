0e8df113aa148834bef89136cb073dddb8e1b0fd
Ryan Reyes <ryan.david.reyes@gmail.com>
2015-06-29 02:59:42 -0400

    Fixed concatenated word.
    /mnt/etc/fstab to /mnt /etc/fstab.

--- arch-deploy.sh
+++ arch-deploy.sh
@@ -4,7 +4,7 @@ pacman -Syu --noconfirm
 pacman -Sy reflector --noconfirm
 cp -vf /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 reflector --verbose --country 'United States' -p http -l 20 --sort rate --save /etc/pacman.d/mirrorlist
-pacstrap -i /mnt base base-devel
-genfstab -p /mnt >> /mnt/etc/fstab
+pacstrap /mnt base base_devel
+genfstab -p /mnt >> /mnt /etc/fstab
 cp arch-chroot-deploy.sh /mnt
 arch-chroot /mnt arch-chroot-deploy.sh
