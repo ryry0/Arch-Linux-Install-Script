403567e953ae04a509f896933b780bdad9f86b01
Ryan Reyes <ryan.david.reyes@gmail.com>
2017-01-09 17:00:01 -0500

    Fixed whitespace bug.

--- arch-chroot-deploy.sh
+++ arch-chroot-deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-echo "Survivor-Arlnx" > /etc/hostname
+echo "aether" > /etc/hostname
 ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 export LANG=en_US.UTF-8
 mkinitcpio -p linux
+pacman -Sy reflector --noconfirm
+cp -vf /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+reflector --verbose --country 'United States' -p http -l 20 --sort rate --save /etc/pacman.d/mirrorlist
 pacman -S dialog wifi-menu os-prober grub iw wpa_supplicant wpa_actiond --noconfirm
-systemctl enable netctl-auto@wlp2s0.service
 pacman -S tlp xorg-server xf86-video-intel git vim-minimal tmux --noconfirm
 passwd
